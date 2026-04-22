4b7a4de74843ddef8dc53c7a1108c88f042e5b39
Carcasona05 <ojcarcasona8@gmail.com>
2026-04-22 22:18:36 +0800

    add files

---
+++ FeedAct/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FeedAct
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FeedAct/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FeedAct
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
